d247fbc2b998a4fdd7e084447c2d8bcc8e57def2
dan.toropov <dan.toropov@gmail.com>
2024-05-01 21:05:06 +0300

    add func two

--- str_func.py
+++ str_func.py
@@ -1,2 +1,15 @@
 def make_capital_letters(text:str) -> str:
+    """
+    home work 3_4
+    :param text:
+    :return:
+    """
+    return ' '.join(x.capitalize() for x in text.split())
+
+def make_capital_letters_again(text:str) -> str:
+    """
+    home work 3_4
+    :param text:
+    :return:
+    """
     return ' '.join(x.capitalize() for x in text.split())
